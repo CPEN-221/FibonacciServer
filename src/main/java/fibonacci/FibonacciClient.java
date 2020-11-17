8d03e6ad777d856d3825d27c51ffee73fcdf9ef7
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2020-11-17 12:24:31 -0800

    updated to gradle

--- src/main/java/fibonacci/FibonacciClient.java
+++ src/main/java/fibonacci/FibonacciClient.java
@@ -11,14 +11,16 @@ import java.net.Socket;
  * at which point it is "closed" and may not be used further.
  */
 public class FibonacciClient {
+    private static final int N = 100;
     private Socket socket;
     private BufferedReader in;
-    private PrintWriter out;
     // Rep invariant: socket, in, out != null
-    
+    private PrintWriter out;
+
     /**
      * Make a FibonacciClient and connect it to a server running on
      * hostname at the specified port.
+     *
      * @throws IOException if can't connect
      */
     public FibonacciClient(String hostname, int port) throws IOException {
@@ -26,9 +28,37 @@ public class FibonacciClient {
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
-    
+
+    /**
+     * Use a FibonacciServer to find the first N Fibonacci numbers.
+     */
+    public static void main(String[] args) {
+        try {
+            FibonacciClient client = new FibonacciClient("35.236.3.212",
+                    FibonacciServer.FIBONACCI_PORT);
+
+            // send the requests to find the first N Fibonacci numbers
+            for (int x = 1; x <= N; ++x) {
+                client.sendRequest(x);
+                System.out.println("fibonacci(" + x + ") = ?");
+            }
+
+            // collect the replies
+            for (int x = 1; x <= N; ++x) {
+                BigInteger y = client.getReply();
+                System.out.println("fibonacci(" + x + ") = " + y);
+            }
+
+            client.close();
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
     /**
      * Send a request to the server. Requires this is "open".
+     *
      * @param x to find Fibonacci(x)
      * @throws IOException if network or server failure
      */
@@ -36,10 +66,11 @@ public class FibonacciClient {
         out.print(x + "\n");
         out.flush(); // important! make sure x actually gets sent
     }
-    
+
     /**
      * Get a reply from the next request that was submitted.
      * Requires this is "open".
+     *
      * @return the requested Fibonacci number
      * @throws IOException if network or server failure
      */
@@ -48,10 +79,11 @@ public class FibonacciClient {
         if (reply == null) {
             throw new IOException("connection terminated unexpectedly");
         }
-        
+
         try {
             return new BigInteger(reply);
-        } catch (NumberFormatException nfe) {
+        }
+        catch (NumberFormatException nfe) {
             throw new IOException("misformatted reply: " + reply);
         }
     }
@@ -59,41 +91,12 @@ public class FibonacciClient {
     /**
      * Closes the client's connection to the server.
      * This client is now "closed". Requires this is "open".
+     *
      * @throws IOException if close fails
      */
     public void close() throws IOException {
         in.close();
         out.close();
         socket.close();
-    }
-    
-    
-    
-    
-    private static final int N = 100;
-    
-    /**
-     * Use a FibonacciServer to find the first N Fibonacci numbers.
-     */
-    public static void main(String[] args) {
-        try {
-            FibonacciClient client = new FibonacciClient("localhost", FibonacciServer.FIBONACCI_PORT);
-
-            // send the requests to find the first N Fibonacci numbers
-            for (int x = 1; x <= N; ++x) {
-                client.sendRequest(x);
-                System.out.println("fibonacci("+x+") = ?");
-            }
-            
-            // collect the replies
-            for (int x = 1; x <= N; ++x) {
-                BigInteger y = client.getReply();
-                System.out.println("fibonacci("+x+") = "+y);
-            }
-            
-            client.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
     }
 }
